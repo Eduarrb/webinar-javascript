d4ea553610441ab3c260f4fbab33354c166f3054
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2021-10-05 11:00:12 -0500

    final

--- 02 final/script.js
+++ 02 final/script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+// document.querySelector('.message').textContent = '✌ Número correcto';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 100;
+
+// document.querySelector('.guess').value = 20;
+
+const numero = Math.trunc(Math.random() * 20) + 1;
+
+let puntaje = 20;
+
+// document.querySelector('.number').textContent = numero;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const numInput = document.querySelector('.guess').value;
+    // console.log(numInput);
+
+    if(!numInput){
+        document.querySelector('.message').textContent = 'No has ingresado ningún número 💥💥';
+    }
+    else if(numInput == numero){
+        document.querySelector('.message').textContent = 'Número correcto ⭐';
+        document.querySelector('body').style.backgroundColor = '#30b347';
+        document.querySelector('.number').textContent = numero;
+    }
+    else if(numInput > numero){
+        if(puntaje > 1){
+            document.querySelector('.message').textContent = 'El número ingresado es muy alto';
+            // puntaje = puntaje - 1;
+            puntaje--;
+            document.querySelector('.score').textContent = puntaje;
+        }
+        else{
+            document.querySelector('.message').textContent = 'Has perdido el juego 😢';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    else if(numInput < numero){
+        if(puntaje > 1){
+            document.querySelector('.message').textContent = 'El número ingresado es muy bajo';
+            // puntaje = puntaje - 1;
+            puntaje--;
+            document.querySelector('.score').textContent = puntaje;
+        }
+        else{
+            document.querySelector('.message').textContent = 'Has perdido el juego 😢';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    document.querySelector('.guess').value = '';
+});
